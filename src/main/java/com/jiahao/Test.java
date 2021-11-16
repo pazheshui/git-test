b78d2668abc97e0fab1b4d29775739d42e97db21
pazheshui <424200659@qq.com>
2021-11-16 21:42:01 +0800

    second commit

--- src/main/java/com/jiahao/Test.java
+++ src/main/java/com/jiahao/Test.java
@@ -1,4 +1,11 @@
 package com.jiahao;
 
 public class Test {
+    public static void main(String[] args) {
+        System.out.println("123");
+        System.out.println("123");
+        System.out.println("123");
+
+
+    }
 }
